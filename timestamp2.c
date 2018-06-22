21e7d0082663b3e0adb2aa397d87af6e3417b768
HeinerBaumgarten <heiner.baumgarten@gmail.com>
2018-06-22 16:10:08 +0200

    Add files via upload

---
+++ timestamp2.c
@@ -0,0 +1,52 @@
+/*
+ ============================================================================
+ Name        : timestamp2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define TS_SIZE 28
+
+void printtimestamp(FILE *fp, struct timeval tp,struct tm *t,char timestamp[TS_SIZE]);
+void settimezone();
+
+int main(void) {
+
+	struct timeval tp;
+	struct tm *t ;
+	char timestamp[] = "YYYY-MM-DD-Thh:mm:ss.mssUTC";
+	FILE *filepointer;
+
+	filepointer = fopen("./test.txt","w+");
+	gettimeofday(&tp, NULL);
+	t = localtime(&tp.tv_sec);
+	settimezone();
+	for(int i= 0; i < 1000; i++){
+		printtimestamp(filepointer,tp,t, timestamp);
+	}
+	fclose(filepointer);
+
+	return EXIT_SUCCESS;
+}
+
+void printtimestamp(FILE *fp,struct timeval tp, struct tm *t,char timestamp[TS_SIZE]){
+
+	gettimeofday(&tp, NULL);
+	t = localtime(&tp.tv_sec);
+	strftime(timestamp,TS_SIZE,"%Y-%m-%dT%H:%M:%S",t);
+	fprintf(fp,"%s.%03d%s\t",timestamp,(int)(tp.tv_usec/1000),t->tm_zone);
+}
+
+void settimezone(){
+	setenv("TZ","UTC0",1);
+	tzset();
+}
